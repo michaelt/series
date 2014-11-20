f79511bbce44c6fe16a77a42c601aa099491f428
michaelt <what_is_it_to_do_anything@yahoo.com>
2014-11-19 19:25:16 -0500

    little tests

--- Main.hs
+++ Main.hs
@@ -2,15 +2,52 @@ import ListM.Types
 import ListM.Combinators
 import ListM.Fusion
 import Prelude hiding (map, filter, drop, take, sum
-                      , iterate, repeat, replicate, splitAt)
+                      , iterate, repeat, replicate
+                      , splitAt, mapM, takeWhile)
+import qualified Prelude as P
 import Data.Functor.Identity
 import Control.Monad.Morph
+import System.Environment
+main = do (a:sn:_) <- getArgs -- pain where
+          let n = 1000 * read sn :: Int
+          case a of  "f" -> f n
+                     "g" -> g n
+                     "x" -> x n
+                     "l" -> l n
+                     _      -> putStrLn "f g x l [Int]" 
+  where
+    f :: Int -> IO ()
+    f n = sumF ( 
+                 (takeWhileF (< n)
+                  (dropF 100
+                    (mapF (\x ->  3*x + 1)
+                    (filterF even
+                   (iterateF (\x -> x+1) (10 :: Int) )
+                 )))) :: ListM (Of Int) IO ())  >>= print
 
-main :: IO ()
-main = sumF ( 
-             (takeF 1000000
-              (dropF 100
-                (mapF (\x -> 3*x + 1)
-                (filterF even
-               (iterateF (\x -> x+1) (10 :: Int) )
-             )))))  >>= print
+    g :: Int -> IO ()
+    g n = print $ sumG ( 
+                 (takeWhileG (< n)
+                  (dropG 100
+                    (mapG (\x ->  3*x + 1)
+                    (filterG even
+                   (iterateG (\x -> x+1) (10 :: Int) )
+                 )))) :: ListM (Of Int) Identity ()) 
+
+    x :: Int -> IO ()
+    x n = sum ( 
+                 (takeWhile (< n)
+                  (drop 100
+                    (map (\x -> 3*x + 1)
+                    (filter even
+                   ((iterate (\x -> x+1) (10 :: Int) ) :: ListM (Of Int) IO ())
+                  )))))  >>= print
+                  
+                  
+    l n = print $ P.sum (
+      (P.takeWhile (< n)
+       (P.drop 100
+         (P.map (\x -> 3*x + 1)
+         (P.filter even
+        ((P.iterate (\x -> x+1) (10 :: Int) ) )
+       )))))  + 1
